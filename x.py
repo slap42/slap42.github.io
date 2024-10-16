ef89e58f13f2a3ba839804cc39397f459470db39
Jack <slap42game@gmail.com>
2024-10-16 22:46:55 +0100

    sdfasdf

--- x.py
+++ x.py
@@ -12,7 +12,7 @@
 from datetime import datetime, timedelta
 
 # XP
-xp_this_hour = sum([  ])
+xp_this_hour = sum([ 78, 60, 94, 57,  ])
 xp_per_hour = [ 
   [ 412, 565, ],
   [ 349, 402, 598, 375, ],
@@ -20,7 +20,7 @@ xp_per_hour = [
   [ 484, 285, 313, ],
   [ 316, 554, 439, 268, 182, ],
   [ 400, 209, 297, 365, ],
-  [ 313, 494, 212, 314, ]
+  [ 313, 494, 212, 314, 232, 200, 272, 333, 354, ]
   ]
 
 # Dates played
